0df3d3af515a8ba40ff4783d523b89f82dce7693
syedadil0803 <129782085+Syedadil0803@users.noreply.github.com>
2025-11-13 09:21:52 +0530

    added

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,9 +1,11 @@
 import HomeComponent from './home'
+import ContactUs from './contact'
 
 function App() {
   return (
     <div className='App'>
     {HomeComponent}
+    {ContactUs}
     </div>
   );
 }
