8aa2694d2714c812b9ce317d983d4e78a500e5ec
syedadil0803 <129782085+Syedadil0803@users.noreply.github.com>
2025-11-18 07:30:40 +0530

    added

--- reactproject/src/home.js
+++ reactproject/src/home.js
@@ -1,11 +1,43 @@
+import { useState, useEffect } from 'react';
 import './home.css'
 
-function HomeComponent(){
-    return(
-    <div className='background'>
-        <h1>Home Page</h1>
-    </div>
-);
+function HomeComponent() {
+
+
+    const [users, setUsers]= useState([]);
+
+
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res)=>res.json())
+        .then((data)=>{
+            setUsers(data);
+        })
+        .catch((err)=>console.log("Error fetching data ;", err))
+    },)
+
+    return (
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone Number</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+                <tr>
+                    <th>1</th>
+                    <td>user.name</td>
+                    <td>user.email</td>
+                    <td>user.phone</td>
+                </tr>
+          
+            </tbody>
+        </table>
+    );
 }
 
 export default HomeComponent;
